cb530d0decfeb56aa1978ed3109de108432f751a
Uchenna Edeh <uedeh@bethel.jw.org>
2023-04-28 15:31:44 -0500

    checking in progress

---
+++ pyside2-tutorial/multi-window/multi_win_ondemand.py
@@ -0,0 +1,42 @@
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+
+import sys
+
+from random import randint
+
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel(f"Another Window {randint(0, 100)}")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.w = None
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
+        else:
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec_()
